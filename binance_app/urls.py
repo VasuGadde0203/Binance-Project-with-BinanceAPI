e2cae35984251459dc304645439444f03ad0decf
VasuGadde0203 <vasugadde0203@gmail.com>
2024-07-07 16:38:28 +0530

    Delete binance_app directory

--- binance_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('download_balances/', views.download_balances, name='download_balances'),
-    path('get_binance_symbols/', views.get_binance_symbols, name='get_binance_symbols')
-]
